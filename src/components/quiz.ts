00d48dcfc6ce7cfa45d199323837be6e64042b51
Francesco Doria <doria5@live.it>
2026-03-18 17:03:58 +0100

    add requirements for the quiz component

---
+++ src/components/quiz.ts
@@ -0,0 +1,13 @@
+// REQUIREMENTS:
+// let's create a quiz component that leverages the select feature of the clack library to create a loop in which 
+// the user is asked a question and he has to select the right answer from a list of options.
+
+// the component is a function that is executed inside the quiz view
+// the component accepts multiple arguments: question: string, options: KnowledgeOption[], correctAnswer: string.
+// each option is an object with properties such as value, label, etc.
+// the correct answer corresponds to the value of the right option.
+// when the user selects an option, the answer will be evaluated inside the component and saved in the state.
+// after a certain amount of tests, the user will be able to see the score for the session.
+export const quiz = async () => {
+    
+}
