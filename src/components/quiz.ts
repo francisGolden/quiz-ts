fce0afd0e80c5ff170c899ce0524bc280f78708c
Francesco Doria <doria5@live.it>
2026-03-20 18:32:13 +0100

    correct or wrong answer switch

--- src/components/quiz.ts
+++ src/components/quiz.ts
@@ -1,6 +1,6 @@
 import { select } from "@clack/prompts";
 // REQUIREMENTS:
-// let's create a quiz component that leverages the select feature of the clack library to create a loop in which 
+// let's create a quiz component that leverages the select feature of the clack library to create a loop in which
 // the user is asked a question and he has to select the right answer from a list of options.
 
 // the component is a function that is executed inside the quiz view
@@ -18,21 +18,37 @@ interface QuizProps {
     id: string;
 }
 
-
 // the correct answer corresponds to the id of the right option.
 // when the user selects an option, the answer will be evaluated inside the component and saved in the state.
 // after a certain amount of tests, the user will be able to see the score for the session.
-export const quiz = async ({question, options, correctAnswer, id}: QuizProps) => {
+export const quiz = async ({
+    question,
+    options,
+    correctAnswer,
+    id,
+}: QuizProps) => {
     try {
         const option = await select({
             message: question,
             options: options.map((option) => {
-                return {value: option.optionId, label: option.optionText, hint: ""}
+                return {
+                    value: option.optionId,
+                    label: option.optionText,
+                    hint: "",
+                };
             }),
         });
-        console.log("selected answer", option)
-        console.log("correct answer: ", correctAnswer)
+        console.log("selected answer", option, typeof option);
+        console.log("correct answer: ", correctAnswer, typeof correctAnswer);
+        switch (option) {
+            case correctAnswer:
+                console.log("right!");
+                break;
+            default:
+                console.log("wrong!");
+                break;
+        }
     } catch (error) {
         console.log(error);
     }
-}
+};
